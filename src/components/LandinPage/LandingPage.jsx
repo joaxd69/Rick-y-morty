c5a64cec5dea109168feb4f114120292132bc9f9
unknown <joakig6@gmail.com>
2023-02-14 22:39:37 -0300

    Add image

--- src/components/LandinPage/LandingPage.jsx
+++ src/components/LandinPage/LandingPage.jsx
@@ -1,15 +1,19 @@
 
 
 import { Link } from 'react-router-dom'
+import style from '../LandinPage/LandingPage.module.css'
 export default function Landinpage(){
     
        return(
      <div>
         <h1>Welcome to Rick y morty App</h1>
+
         <Link to='/home'>
-          <button >go to home</button>
+          <button className={style.buttons}>go to home</button>
         </Link>
- 
+        <div className={style.container}>
+        
+        </div>
     </div>   
     )
     
